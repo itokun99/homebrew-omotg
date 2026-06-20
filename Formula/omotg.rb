49141d27bbcbced240f602471470cd0f80f4a8e0
Indrawan Lisanto <ito@localhost.localdomain>
2026-06-21 07:13:46 +0800

    omotg: update license to CC-BY-NC-4.0

--- Formula/omotg.rb
+++ Formula/omotg.rb
@@ -3,7 +3,7 @@ class Omotg < Formula
   homepage "https://github.com/itokun99/omotg"
   url "https://github.com/itokun99/omotg/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-  license "MIT"
+  license "CC-BY-NC-4.0"
 
   depends_on "go" => :build
 
